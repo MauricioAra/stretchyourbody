45cb24d271e1e11289dcf75bd31a5e53384e21c5
mauricioara <mauricioher.94@hotmail.com>
2017-06-28 11:40:35 -0600

    Initial project

---
+++ app/src/main/java/com/strechyourbody/rammp/stretchbody/Activities/MainActivity.java
@@ -0,0 +1,93 @@
+package com.strechyourbody.rammp.stretchbody.Activities;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.strechyourbody.rammp.stretchbody.Fragments.DashBoardFragment;
+import com.strechyourbody.rammp.stretchbody.Fragments.MyProgramFragment;
+import com.strechyourbody.rammp.stretchbody.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setToolbar();
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.navview);
+        setFragmentByDefault();
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                boolean fragmentTransaction = false;
+                Fragment fragment = null;
+
+                switch (item.getItemId()){
+
+                    case R.id.menu_my_program:
+                        fragment = new MyProgramFragment();
+                        fragmentTransaction = true;
+                        break;
+                    case R.id.menu_dash_board:
+                        fragment = new DashBoardFragment();
+                        fragmentTransaction = true;
+                        break;
+                }
+
+
+                if(fragmentTransaction){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.content_frame,fragment).commit();
+                    item.setChecked(true);
+                    getSupportActionBar().setTitle(item.getTitle());
+                    drawerLayout.closeDrawers();
+                }
+
+
+                return true;
+            }
+        });
+
+    }
+
+    private void setToolbar(){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toobar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_home);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void setFragmentByDefault(){
+        getSupportFragmentManager().beginTransaction().replace(R.id.content_frame,new DashBoardFragment()).commit();
+
+        MenuItem item = navigationView.getMenu().getItem(1);
+        item.setChecked(true);
+        getSupportActionBar().setTitle(item.getTitle());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case android.R.id.home:
+                // abrir el menu lateral
+                drawerLayout.openDrawer(GravityCompat.START);
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
